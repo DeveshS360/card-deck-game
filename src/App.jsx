04064dde2717632b5f4ff215dba473a9847cbc9b
Devesh Sharma <34626595+DeveshS360@users.noreply.github.com>
2024-03-11 06:36:48 +0000

    unused code removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import { CardGame } from "./components";
 import "./styles.css";
 
 export default function App() {
-  console.log(
-    [{ name: "Mohit" }, { name: "Mohit" }].indexOf({ name: "Mohit" })
-  );
-
-  for (var i = 0; i < 4; i++) {
-    const callLog = (i) => {
-      setTimeout(() => console.log(i), 0);
-    };
-
-    callLog(i);
-  }
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
